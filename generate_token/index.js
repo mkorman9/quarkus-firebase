7b5ebfa215fdaf6a60c45be41fb996641a08af24
Michal Korman <michal.korman@icloud.com>
2023-10-18 17:37:54 +0200

    format generate_token script

--- generate_token/index.js
+++ generate_token/index.js
@@ -1,28 +1,28 @@
-import { initializeApp } from 'firebase/app';
-import { connectAuthEmulator, getAuth, signInAnonymously } from 'firebase/auth';
-import { readFile } from 'fs/promises';
+import {initializeApp} from 'firebase/app';
+import {connectAuthEmulator, getAuth, signInAnonymously} from 'firebase/auth';
+import {readFile} from 'fs/promises';
 
 const profile = process.argv[2] || 'emulator';
 
 readFile(`${profile}.json`)
-    .then(data => {
-        const firebaseConfig = JSON.parse(data.toString());
-        const app = initializeApp(firebaseConfig);
-        const auth = getAuth(app);
+  .then(data => {
+    const firebaseConfig = JSON.parse(data.toString());
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
 
-        if (profile === 'emulator') {
-            connectAuthEmulator(auth, 'http://127.0.0.1:9099');
-        }
+    if (profile === 'emulator') {
+      connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+    }
 
-        signInAnonymously(auth)
-            .then(credentials => {
-                console.log(`uid = ${credentials.user.uid}`);
-                return credentials.user.getIdToken();
-            })
-            .then(token => console.log(`token = ${token}`))
-            .catch(e => console.error(e));
-    })
-    .catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+    signInAnonymously(auth)
+      .then(credentials => {
+        console.log(`uid = ${credentials.user.uid}`);
+        return credentials.user.getIdToken();
+      })
+      .then(token => console.log(`token = ${token}`))
+      .catch(e => console.error(e));
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });

5c718d64514eaa381b90428ce0280bc8bf478087
Michal Korman <michal.korman@icloud.com>
2023-10-26 00:56:06 +0200

    add retention and target to FirebaseClaim

--- src/test/java/com/github/mkorman9/firebaseintegration/firebase/auth/FirebaseClaim.java
+++ src/test/java/com/github/mkorman9/firebaseintegration/firebase/auth/FirebaseClaim.java
@@ -1,5 +1,12 @@
 package com.github.mkorman9.firebaseintegration.firebase.auth;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface FirebaseClaim {
     String name();
 
